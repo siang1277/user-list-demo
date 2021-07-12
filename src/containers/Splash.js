f725bbc77b2b97e62c6dd4e3139171fcca81138a
siang1277 <weisiang91@gmail.com>
2021-07-12 14:20:28 +0800

    splash

--- src/containers/Splash.js
+++ src/containers/Splash.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import {
     View,
     Text,
+    Image,
+    StyleSheet,
 } from 'react-native';
 import AppContainer from '../components/common/AppContainer';
 import NavigationService from '../navigator/NavigationService';
@@ -9,17 +11,19 @@ import NavigationService from '../navigator/NavigationService';
 const Splash = (props) => {
     useEffect(() => {
         // setTimeout(() => {
-        //     NavigationService.navigate('UserList')
+        //     NavigationService.reset('UserList')
         // }, 100);
     }, [])
 
     return (
         <AppContainer
-            fullScreenEnabled
-        >
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Splash</Text>
-        </View>
+            fullScreenEnabled>
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    source={require('../assets/images/app_logo_text.png')}
+                    resizeMode={'center'}
+                />
+            </View>
         </AppContainer>
     )
 }
